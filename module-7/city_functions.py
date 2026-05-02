5fb8bd417c7a018223e52923c643b92c26635d10
drkboris <lewstherindragon@gmail.com>
2026-05-02 10:24:16 -0500

    Module 7 Assignment Upload

---
+++ module-7/city_functions.py
@@ -0,0 +1,34 @@
+'''
+============================================
+# Title: Assignment 7.1
+# Author: Zach Donohue
+# Date: 30 April 2026
+===========================================
+Title: city_functions.py
+Author: Eric Matthes
+Date: 20 March 2026
+Modified By: Zach Donohue
+Description: This program is used provide functions for the test_cities.py program to test
+============================================
+'''
+
+def city_country(city, country, population=None, language=None):
+    result = f"{city}, {country}"
+    if population:
+        result += f" - population {population}"
+    if language:
+        result += f", {language}"
+    return result
+
+
+# City, Country only
+print(city_country("Santiago", "Chile"))
+
+# City, Country, Population
+print(city_country("New York City", "United States", 8600000000))
+
+# City, Country, Population, Language
+print(city_country("Dublin", "Ireland", 1500000, "Gaeilge"))
+
+
+
---
+++ module-7/test_cities.py
@@ -0,0 +1,27 @@
+'''
+============================================
+# Title: Assignment 7.1
+# Author: Zach Donohue
+# Date: 30 April 2026
+===========================================
+Title: test_cities.py
+Author: Eric Matthes
+Date: 20 March 2026
+Modified By: Zach Donohue
+Description: This program is used to test the city_functions.py program
+============================================
+'''
+
+import unittest
+from city_functions import city_country
+
+
+class TestCityCountry(unittest.TestCase):
+
+    def test_city_country(self):
+        result = city_country("Santiago", "Chile")
+        self.assertEqual(result, "Santiago, Chile")
+
+
+if __name__ == "__main__":
+    unittest.main()
