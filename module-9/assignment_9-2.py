d99d2a228e67e51c967ba09fc2658403701632e9
drkboris <lewstherindragon@gmail.com>
2026-05-17 13:30:41 -0500

    Final upload

--- module-9/assignment_9-2.py
+++ module-9/assignment_9-2.py
@@ -1,7 +1,8 @@
 '''
 ===========================================
 Title: assignment_9-2.py
-Author: Zach Donohue
+API Tutorial: https://www.dataquest.io/blog/api-in-python/
+Author: Charlie Custer
 Date: 14 May 2026
 Modified By: Zach Donohue
 Directions:  The first is to work through a tutorial on APIs then to write
@@ -9,6 +10,7 @@ a Python program using an API of your choice. Couple of things to consider
 when you work with APIs; it's best to test the connection before you start
 working with the response. For example, open up a new python file and use
 this code (save before running):
+
 
 Using the Open Trivia Database - https://opentdb.com
 No API key needed which makes it easy to work with
@@ -28,7 +30,7 @@ def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-# step 1 - test the connection
+# Task 6(b) - test the connection
 url = "https://opentdb.com/api.php"
 params = {
     "amount": 5,
@@ -39,8 +41,8 @@ params = {
 response = requests.get(url, params=params)
 print("Status Code:", response.status_code)
 
-# step 2 - print the raw response with no formatting
+# Task 6(c) - print the raw response with no formatting
 print(response.json())
 
-# step 3 - print the formatted version using jprint
+# Task 6(d) - print the formatted version using jprint
 jprint(response.json())
